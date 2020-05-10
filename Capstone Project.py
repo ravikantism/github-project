5a6c46a416cb5ef9cf7e55c6fdf66ba0dfd25677
ravikantism <65103441+ravikantism@users.noreply.github.com>
2020-05-10 01:47:45 -0400

    Add files via upload

---
+++ Capstone Project.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+## This notebook is for Data Science Capstone Project
+
+
+# In[7]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[6]:
+
+
+print ("Hello Capstone Project Course")
+
+
+# In[ ]:
+
+
+
+
